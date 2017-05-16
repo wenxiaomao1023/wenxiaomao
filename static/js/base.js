a4340059d0e43e8db316bf1fca8f1abee3485f5b
wen <wen@wen>
2017-05-16 11:53:31 +0800

    update

--- static/js/base.js
+++ static/js/base.js
@@ -27,3 +27,29 @@ function existBlank(elementId) {
 	}
 	//obj.value = str.replace(/\s/g, ""); // 这句话可以强制删除所有空格
 }
+
+function htmlEncode(str){   
+  var s = "";   
+  if (str.length == 0) return "";   
+  s = str.replace(/&/g, "&gt;");   
+  s = s.replace(/</g, "&lt;");   
+  s = s.replace(/>/g, "&gt;");   
+  s = s.replace(/ /g, "&nbsp;");   
+  s = s.replace(/\'/g, "&#39;");   
+  s = s.replace(/\"/g, "&quot;");   
+  s = s.replace(/\n/g, "<br>");   
+  return s;   
+}   
+ 
+function htmlDecode(str){   
+  var s = "";   
+  if (str.length == 0) return "";   
+  s = str.replace(/&gt;/g, "&");   
+  s = s.replace(/&lt;/g, "<");   
+  s = s.replace(/&gt;/g, ">");   
+  s = s.replace(/&nbsp;/g, " ");   
+  s = s.replace(/&#39;/g, "\'");   
+  s = s.replace(/&quot;/g, "\"");   
+  s = s.replace(/<br>/g, "\n");   
+  return s;   
+}
